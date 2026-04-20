07f0b6d377e4bf3cb5915ea4d0e3f72339a81bdc
ZukhrufianAbdullah <zukrufianabdullah@gmail.com>
2026-04-20 13:38:09 +0700

    Membuat Menu Stok

---
+++ app/Filament/Resources/Stoks/Schemas/StokForm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Resources\Stoks\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+use Illuminate\Support\Facades\Auth;
+
+class StokForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Select::make('supplier_id')
+                    ->relationship('supplier', 'supplier_nama')
+                    ->required(),
+                Select::make('barang_id')
+                    ->relationship('barang', 'barang_nama')
+                    ->required(),
+                DateTimePicker::make('stok_tanggal')
+                    ->required()
+                    ->default(now()),
+                TextInput::make('stok_jumlah')
+                    ->required()
+                    ->numeric()
+                    ->minValue(1),
+                Hidden::make('user_id')
+                    ->default(Auth::id()),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Stoks/Tables/StoksTable.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources\Stoks\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreBulkAction;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Actions\RestoreAction;
+use Filament\Actions\ForceDeleteAction;
+
+class StoksTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('stok_id')
+                    ->label('ID')
+                    ->sortable(),
+                TextColumn::make('supplier.supplier_nama')
+                    ->label('Supplier')
+                    ->sortable(),
+                TextColumn::make('barang.barang_nama')
+                    ->label('Barang')
+                    ->sortable(),
+                TextColumn::make('stok_jumlah')
+                    ->label('Jumlah')
+                    ->sortable(),
+                TextColumn::make('user.nama')
+                    ->label('User')
+                    ->sortable(),
+                TextColumn::make('stok_tanggal')
+                    ->label('Tanggal')
+                    ->dateTime('d M Y H:i')
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->label('Dibuat')
+                    ->dateTime('d M Y H:i')
+                    ->sortable(),
+                TextColumn::make('updated_at')
+                    ->label('Diupdate') 
+                    ->dateTime('d M Y H:i')
+                    ->sortable(),
+                TextColumn::make('deleted_at')
+                    ->label('Dihapus')
+                    ->dateTime('d M Y H:i')
+                    ->sortable(),
+            ])
+            ->filters([
+                TrashedFilter::make(),
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+                RestoreAction::make(),
+                ForceDeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+}
